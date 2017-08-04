e267e05c35b03525df30234983d5e3e3eb333602
Adam Grare <agrare@redhat.com>
2017-08-04 09:17:27 -0400

    Add arg parsing and run as a worker

--- metric/metrics_collector.rb
+++ metric/metrics_collector.rb
@@ -1,17 +1,119 @@
 require "logger"
 require "csv"
+require "rbvmomi/vim"
 
 class MetricsCollector
-  attr_reader :vim
-  def initialize(vim)
-    @vim = vim
+  attr_reader :hostname, :username, :password, :collect_interval, :exit_requested
+  def initialize(hostname, username, password, collect_interval = 60)
+    @hostname = hostname
+    @username = username
+    @password = password
+
+    @collect_interval = collect_interval
+    @exit_requested = false
+  end
+
+  def run
+    conn = connect(hostname, username, password)
+
+    # At startup request the list of all counters from the PerformanceManager
+    perf_counters_by_name = {}
+    perf_counter_info(conn).to_a.each do |counter|
+      perf_counters_by_name[perf_counter_key(counter)] = counter
+    end
+
+    perf_counters_to_collect = METRIC_CAPTURE_COUNTERS.collect do |counter_name|
+      perf_counters_by_name[counter_name]
+    end
+
+    start_time = nil
+
+    until exit_requested
+      vms = get_all_powered_on_vms(conn)
+      entity_metrics = perf_query(conn, perf_counters_to_collect, vms, format: "csv", start_time: start_time)
+      start_time = Time.now
+
+      sleep(collect_interval)
+    end
+
+    log.info("Exiting...")
+  ensure
+    conn.close unless conn.nil?
+  end
+
+  def stop
+    log.info("Exit requested...")
+    @exit_requested = true
+  end
+
+  private
+
+  METRIC_CAPTURE_COUNTERS = [
+    :cpu_usage_rate_average,
+    :cpu_usagemhz_rate_average,
+    :mem_usage_absolute_average,
+    :disk_usage_rate_average,
+    :net_usage_rate_average,
+    :sys_uptime_absolute_latest,
+    :cpu_ready_delta_summation,
+    :cpu_system_delta_summation,
+    :cpu_wait_delta_summation,
+    :cpu_used_delta_summation,
+    :mem_vmmemctl_absolute_average,
+    :mem_vmmemctltarget_absolute_average,
+    :mem_swapin_absolute_average,
+    :mem_swapout_absolute_average,
+    :mem_swapped_absolute_average,
+    :mem_swaptarget_absolute_average,
+    :disk_devicelatency_absolute_average,
+    :disk_kernellatency_absolute_average,
+    :disk_queuelatency_absolute_average
+  ].freeze
+
+  def connect(host, username, password)
+    log.info("Connecting to #{host}...")
+
+    opts = {
+      :ns       => "urn:vim25",
+      :host     => host,
+      :ssl      => true,
+      :insecure => true,
+      :path     => "/sdk",
+      :port     => 443,
+      :rev      => "6.5",
+    }
+
+    require 'rbvmomi/vim'
+
+    conn = RbVmomi::VIM.new(opts).tap do |vim|
+      vim.rev = vim.serviceContent.about.apiVersion
+
+      log.info("Logging in to #{username}@#{host}...")
+      vim.serviceContent.sessionManager.Login(
+        :userName => username,
+        :password => password,
+      )
+      log.info("Logging in to #{username}@#{host}...Complete")
+    end
+
+    log.info("Connected...")
+    conn
   end
 
   def log
     @logger ||= Logger.new(STDOUT)
   end
 
-  def perf_counter_info
+  def perf_counter_key(counter)
+    group  = counter.groupInfo.key.downcase
+    name   = counter.nameInfo.key.downcase
+    rollup = counter.rollupType.downcase
+    stats  = counter.statsType.downcase
+
+    "#{group}_#{name}_#{stats}_#{rollup}".to_sym
+  end
+
+  def perf_counter_info(vim)
     log.info("Retrieving perf counters...")
 
     spec_set = [
@@ -46,7 +148,7 @@ class MetricsCollector
     perf_counters
   end
 
-  def perf_query(perf_counters, entities, interval: "20", start_time: nil, end_time: nil, format: "normal", max_sample: nil)
+  def perf_query(vim, perf_counters, entities, interval: "20", start_time: nil, end_time: nil, format: "normal", max_sample: nil)
     log.info("Collecting performance counters...")
 
     format = RbVmomi::VIM.PerfFormat(format)
@@ -82,5 +184,51 @@ class MetricsCollector
     log.info("Collecting performance counters...Complete")
 
     all_metrics
+  end
+
+  def get_all_powered_on_vms(conn)
+    get_all_vms(conn, ["runtime.powerState"]).collect do |vm, props|
+      power_state = props.to_a.detect { |p| p.name == "runtime.powerState" }.val.to_s
+      next unless power_state == "poweredOn"
+      vm
+    end.compact
+  end
+
+  def get_all_vms(conn, path_set = [])
+    filter_spec = RbVmomi::VIM.PropertyFilterSpec(
+      :objectSet => [
+        :obj => conn.rootFolder,
+        :selectSet => [
+          RbVmomi::VIM.TraversalSpec(
+            :name => 'tsFolder',
+            :type => 'Folder',
+            :path => 'childEntity',
+            :skip => false,
+            :selectSet => [
+              RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
+              RbVmomi::VIM.SelectionSpec(:name => 'tsDatacenterVmFolder'),
+            ]
+          ),
+          RbVmomi::VIM.TraversalSpec(
+            :name => 'tsDatacenterVmFolder',
+            :type => 'Datacenter',
+            :path => 'vmFolder',
+            :skip => false,
+            :selectSet => [
+              RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')
+            ]
+          )
+        ],
+      ],
+      :propSet => [
+        RbVmomi::VIM.PropertySpec(
+          :type => "VirtualMachine",
+          :pathSet => path_set,
+        )
+      ]
+    )
+
+    result = conn.propertyCollector.RetrieveProperties(:specSet => [filter_spec])
+    result.to_a.collect { |r| [r.obj, r.propSet] }
   end
 end
--- metric/worker.rb
+++ metric/worker.rb
@@ -1,98 +1,37 @@
-require "rbvmomi/vim"
+require "trollop"
 require_relative "metrics_collector"
 
-metric_capture_counters = [
-  :cpu_usage_rate_average,
-  :cpu_usagemhz_rate_average,
-  :mem_usage_absolute_average,
-  :disk_usage_rate_average,
-  :net_usage_rate_average,
-  :sys_uptime_absolute_latest,
-  :cpu_ready_delta_summation,
-  :cpu_system_delta_summation,
-  :cpu_wait_delta_summation,
-  :cpu_used_delta_summation,
-  :mem_vmmemctl_absolute_average,
-  :mem_vmmemctltarget_absolute_average,
-  :mem_swapin_absolute_average,
-  :mem_swapout_absolute_average,
-  :mem_swapped_absolute_average,
-  :mem_swaptarget_absolute_average,
-  :disk_devicelatency_absolute_average,
-  :disk_kernellatency_absolute_average,
-  :disk_queuelatency_absolute_average
-].freeze
+def main args
+  collector = MetricsCollector.new(args[:hostname], args[:user], args[:password])
 
-def connect
-  RbVmomi::VIM.connect(
-    :host => ENV["EMS_HOSTNAME"],
-    :user => ENV["EMS_USERNAME"],
-    :password => ENV["EMS_PASSWORD"],
-    :ssl => true,
-    :insecure => true,
-  )
+  thread = Thread.new { collector.run }
+
+  begin
+    loop { sleep 1 }
+  rescue Interrupt
+    collector.stop
+    thread.join
+  end
 end
 
-def all_vms(conn)
-  filter_spec = RbVmomi::VIM.PropertyFilterSpec(
-    :objectSet => [
-      :obj => conn.rootFolder,
-      :selectSet => [
-        RbVmomi::VIM.TraversalSpec(
-          :name => 'tsFolder',
-          :type => 'Folder',
-          :path => 'childEntity',
-          :skip => false,
-          :selectSet => [
-            RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
-            RbVmomi::VIM.SelectionSpec(:name => 'tsDatacenterVmFolder'),
-          ]
-        ),
-        RbVmomi::VIM.TraversalSpec(
-          :name => 'tsDatacenterVmFolder',
-          :type => 'Datacenter',
-          :path => 'vmFolder',
-          :skip => false,
-          :selectSet => [
-            RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')
-          ]
-        )
-      ],
-    ],
-    :propSet => [RbVmomi::VIM.PropertySpec(
-      :type => "VirtualMachine",
-      :pathSet => ["runtime.powerState"]
-    )]
-  )
+def parse_args
+  args = Trollop.options do
+    opt :hostname, "hostname", :type => :string
+    opt :user,     "username", :type => :string
+    opt :password, "password", :type => :string
+  end
 
-  result = conn.propertyCollector.RetrieveProperties(:specSet => [filter_spec])
-  result.to_a.collect do |r|
-    next if r.propSet.first.val == "poweredOff"
-    r.obj
-  end.compact
+  args[:hostname] ||= ENV["EMS_HOSTNAME"]
+  args[:user]     ||= ENV["EMS_USERNAME"]
+  args[:password] ||= ENV["EMS_PASSWORD"]
+
+  raise Trollop::CommandlineError, "--hostname required" if args[:hostname].nil?
+  raise Trollop::CommandlineError, "--user required"     if args[:user].nil?
+  raise Trollop::CommandlineError, "--password required" if args[:password].nil?
+
+  args
 end
 
-conn = connect
+args = parse_args
 
-vms = all_vms(conn)
-
-puts "Retrieving metrics for #{vms.count}"
-
-collector = MetricsCollector.new(conn)
-
-perf_counters_by_name = {}
-collector.perf_counter_info.to_a.each do |counter|
-  group  = counter.groupInfo.key.downcase
-  name   = counter.nameInfo.key.downcase
-  rollup = counter.rollupType.downcase
-  stats  = counter.statsType.downcase
-
-  perf_counter_name = "#{group}_#{name}_#{stats}_#{rollup}".to_sym
-  perf_counters_by_name[perf_counter_name] = counter
-end
-
-counters_to_collect = metric_capture_counters.map do |counter_name|
-  perf_counters_by_name[counter_name]
-end
-
-metrics = collector.perf_query(counters_to_collect, vms, format: "csv")
+main args
